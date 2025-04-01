beea07894e15a636c1fe9e73ed3c43ea4c3cc021
bell1005 <imoto.ouki.05@icloud.com>
2025-04-01 10:59:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.getElementById("approvalLink").addEventListener("click", function () {
-    document.getElementById("messageBox").classList.remove("hidden");
-});
+// 「はい」を押したときの処理
+function approve() {
+  showResult([
+    "いつも頑張ってて偉い！",
+    "かっこいい！",
+    "いつも助かってるよ！"
+  ]);
+}
 
-document.getElementById("yesButton").addEventListener("click", function () {
-    document.getElementById("messageBox").classList.add("hidden");
-    document.getElementById("responseBox").innerHTML = `
-        <ul>
-            <li>いつも頑張ってて偉い！</li>
-            <li>かっこいい！</li>
-            <li>いつも助かってるよ！</li>
-        </ul>
-    `;
-    document.getElementById("responseBox").classList.remove("hidden");
-});
+// 「いいえ」を押したときの処理
+function deny() {
+  showResult([
+    "ﾆｨｨｲｲｲｲｲｲｲｲｲｲｲ",
+    "ｸﾞｧﾝｸﾞｧﾝｸﾞｧﾝｸﾞｧﾝ",
+    "ﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞ"
+  ]);
+}
 
-document.getElementById("noButton").addEventListener("click", function () {
-    document.getElementById("messageBox").classList.add("hidden");
-    document.getElementById("responseBox").innerHTML = `
-        <ul>
-            <li>ﾆｨｨｲｲｲｲｲｲｲｲｲｲｲ</li>
-            <li>ｸﾞｧﾝｸﾞｧﾝｸﾞｧﾝｸﾞｧﾝ</li>
-            <li>ﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞｼﾞﾍﾞ</li>
-        </ul>
-    `;
-    document.getElementById("responseBox").classList.remove("hidden");
-});
+// 結果を表示する関数
+function showResult(messages) {
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = messages.map(msg => `<p>${msg}</p>`).join('');
+  resultDiv.style.display = 'block';
+  document.querySelector('.container').style.height = 'auto'; // 高さ自動調整
+}
